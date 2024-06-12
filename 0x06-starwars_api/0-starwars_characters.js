79c1cce3cae979eadca4975a592fd1af888d3cb4
Tamara Lumumba <tlumumba@gmail.com>
2024-06-12 21:31:18 +0300

    Changes

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,27 +1,23 @@
 #!/usr/bin/node
-
 const request = require('request');
 
-const filmNum = process.argv[2] + '/';
-const filmURL = 'https://swapi-api.hbtn.io/api/films/';
+const endpoint = 'https://swapi-api.hbtn.io/api';
+const filmId = process.argv[2];
 
-// Makes an API request to get film information
-request(filmURL + filmNum, async function (err, res, body) {
-  if (err) return console.error(err);
+request(`${endpoint}/films/${filmId}/`, async function (error, response, body) {
+  if (error) return console.log(error);
 
-  // Parse the response body to get the list of character URLs
-  const charURLList = JSON.parse(body).characters;
+  let characters = JSON.parse(body).characters;
 
-  // Iterare through the character URLs and fect character information
-  // Make a request to each character URL
-  for (const charURL of charURLList) {
-    await new Promise(function (resolve, reject) {
-      request(charURL, function (err, res, body) {
-        if (err) return console.error(err);
-
-        // Parse the charcter nformation and print the character's name Resolve the promise to indicate completion
-        console.log(JSON.parse(body).name);
-        resolve();
+  for (const character of characters) {
+    await new Promise((resolve, reject) => {
+      request(character, (error, response, body) => {
+        if (error) {
+          reject(error);
+        } else {
+          console.log(JSON.parse(body).name);
+          resolve(body);
+        }
       });
     });
   }
